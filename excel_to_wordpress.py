76834c2f7649105b9b455f26adac79437bae6829
Jankin.Li <jankin.li@outlook.com>
2018-08-23 22:22:40 +0800

    update content

--- excel_to_wordpress.py
+++ excel_to_wordpress.py
@@ -1,5 +1,9 @@
-#!/usr/bin/python
 # -*- coding: UTF-8 -*-
+#!/usr/bin/python
+import sys;
+if sys.getdefaultencoding() != 'utf-8':
+    reload(sys);
+    sys.setdefaultencoding('utf-8');
 
 import xlrd;
 import MySQLdb;
@@ -37,12 +41,13 @@ def parseData(sheetSrc):
                 if num > 1:
                     postNewPost(num, rowList);
                     #if( num > 3) :
-                    #break;
+                    break;
 
 def postNewPost(order, data):
     print("postNewPost begin. order="+ str(order));
     if (len(data) < 5):
         return;
+
     title="";
     content="";
     enterName="";
@@ -52,18 +57,45 @@ def postNewPost(order, data):
     contactTel="";
     contactEmail="";
     completeDate="";
+    applyStatus="";
+    cowork_type=""; #合作方式
+    apply_score=""; #成果应用行业
+    result_type=""; #成果形式
+    award_type=""; #获奖类别 10
+    award_level="";  #获奖级别 11
+    prospect_promotion="";  #推广前景   12
+    patent_name="";  #专利名称  13
+    postal_address="";  #通讯地址 18
+    district="";    #所在地区  19
+
 
     for cell in data:
         if cell.has_key(1) :
             title = cell.get(1);
-        if cell.has_key(4) :
-            content = cell.get(4);
         if( cell.has_key(2)) :
             enterName = cell.get(2);
         if (cell.has_key(3)):
             techField = cell.get(3);
+        if cell.has_key(4) :
+            content = cell.get(4);
         if (cell.has_key(5)):
             techMaturity = cell.get(5);
+        if (cell.has_key(6)):
+            applyStatus = cell.get(6);
+        if cell.has_key(7) :
+            cowork_type = cell.get(7);
+        if cell.has_key(8) :
+            apply_score = cell.get(8);
+        if cell.has_key(9) :
+            result_type = cell.get(9);
+        if cell.has_key(10):
+            award_type = cell.get(10);  # 获奖类别 10
+        if cell.has_key(11):
+            award_level = cell.get(11);  # 获奖级别 11
+        if cell.has_key(12):
+            prospect_promotion = cell.get(12);  # 推广前景   12
+        if cell.has_key(13):
+            patent_name = cell.get(13);  # 专利名称  13
         if (cell.has_key(14)):
             contactName = cell.get(14);
         if (cell.has_key(15)):
@@ -72,14 +104,23 @@ def postNewPost(order, data):
             contactEmail = cell.get(16);
         if cell.has_key( 17) :
             completeDate = cell.get(17);
-    postNewPostByXmlRpc(title, content,enterName,techField,techMaturity,contactName,contactTel,contactEmail,completeDate);
+        if cell.has_key(18):
+            postal_address = cell.get(18);  # 通讯地址 18
+        if cell.has_key(19):
+            district = cell.get(19);  # 所在地区  19
+    postNewPostByXmlRpc(title, content,enterName,techField,techMaturity,contactName,contactTel,contactEmail,completeDate,
+                        applyStatus,cowork_type,apply_score,result_type,award_type,award_level,prospect_promotion,
+                        patent_name,postal_address,district);
 
-def postNewPostByXmlRpc(title, content, enterName,techField,techMaturity,contactName,contactTel,contactEmail,completeDate):
+def postNewPostByXmlRpc(title, content, enterName,techField,techMaturity,contactName,contactTel,contactEmail,completeDate,
+                        applyStatus, cowork_type, apply_score, result_type, award_type, award_level, prospect_promotion,
+                        patent_name, postal_address, district):
     print("postNewPostByXmlRpc");
     wp = Client('http://39.106.104.45/wordpress/xmlrpc.php', 'shikun', 'ShiKun001')
     post = WordPressPost();
     post.title = title;
-    post.content = content;
+    post.content = gen_content(content, enterName,applyStatus,apply_score, result_type, award_type, award_level,
+                               prospect_promotion,patent_name,postal_address);
     post.terms_names = {
         'post_tag': ['北京理工大学'],
         'category': ['成果展示']
@@ -95,9 +136,67 @@ def postNewPostByXmlRpc(title, content, enterName,techField,techMaturity,contact
     post.id = wp.call(NewPost(post));
     print("post.id = " + str(post.id));
     postId = post.id;
-    insertOtherDataIntoDB(postId,enterName,techField,techMaturity,contactName,contactTel,contactEmail,completeDate);
+    insertOtherDataIntoDB(postId,enterName,techField,techMaturity,contactName,contactTel,contactEmail,completeDate,
+                          cowork_type,district);
 
-def insertOtherDataIntoDB(postId,enterName,techField,techMaturity,contactName,contactTel,contactEmail,completeDate) :
+def gen_content(content, enterName,applyStatus,apply_score, result_type, award_type, award_level, prospect_promotion,
+                patent_name,postal_address):
+    str="""<h3>成果介绍</h3>""".encode(encoding='utf-8');
+    str+="""<p>""".encode(encoding='utf-8');
+    str+=content;
+    str+="""</p>""".encode(encoding='utf-8');
+    str+="""<hr />""".encode(encoding='utf-8');
+    str+="""<table border="1" cellspacing="0" cellpadding="5">
+<tbody>
+<tr>""".encode(encoding='utf-8');
+    str += """<td><b>完成单位:</b>""".encode(encoding='utf-8');
+    str+=enterName;
+    str+="""</td>
+<td><b>应用情况:</b>""".encode(encoding='utf-8');
+    str+=applyStatus;
+    str+="""</td>
+</tr>
+<tr>""".encode(encoding='utf-8');
+    str+="""<td><b>获奖类别:</b>""".encode(encoding='utf-8');
+    str+=award_type;
+    str+="""</td>
+<td><b>获奖级别:</b>""".encode(encoding='utf-8');
+    str+=award_level;
+    str+="""</td>
+</tr>
+<tr>
+<td colspan="2"><b>成果应用行业:</b>""".encode(encoding='utf-8');
+    str+=apply_score;
+    str+="""<br>
+</td>
+</tr>
+<tr>
+<td colspan="2"><b>成果形式:</b><br>""".encode(encoding='utf-8');
+    str+=result_type;
+    str+="""</td>
+</tr>
+<tr>
+<td colspan="2"><b>推广前景:</b><br>""".encode(encoding='utf-8');
+    str+=prospect_promotion;
+    str+="""</td>
+</tr>
+<tr>
+<td colspan="2"><b>专利名称:</b><br>""".encode(encoding='utf-8');
+    str += patent_name;
+    str += """</td>
+    </tr>
+    <tr>
+    <td colspan="2"><b>通讯地址:</b><br>""".encode(encoding='utf-8');
+    str += postal_address;
+    str += """</td>
+    </tr>
+</tbody>
+</table>
+    """.encode(encoding='utf-8');
+    return str;
+
+def insertOtherDataIntoDB(postId,enterName,techField,techMaturity,contactName,contactTel,contactEmail,completeDate,
+                          cowork_type, district) :
     print("insertOtherDataIntoDB  postId= " + str(postId));
     if len(enterName) > 0:
         insertMeta(postId, "enter-name", enterName);
@@ -114,7 +213,11 @@ def insertOtherDataIntoDB(postId,enterName,techField,techMaturity,contactName,co
         insertMeta(postId, "contact-email", contactEmail);
     if len(completeDate)> 0 :
         print("tech-date="+completeDate);
-        inserMeta(postId, "tech-date", completeDate);
+        insertMeta(postId, "tech-date", completeDate);
+    if len(cowork_type)>0:
+        insertMeta(postId, "cowork-type", cowork_type);
+    if len(district)>0 :
+        insertMeta(postId, "district_name", district);
 
 def insertMeta(postId, key, value):
     print("insertMeta begin.")
